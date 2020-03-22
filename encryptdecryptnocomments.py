485cd2935e8015c0c3e5c46c1565b4eb12b0768b
Idiot Cheese <marius.nh.1014@gmail.com>
2020-03-22 12:28:00 +0100

    encryptdecrypt uden kommentarer
    rettelser, og et dokument der måske skal tilføjes som bilag, så jeg har fjernet kommentarerne.

--- encryptdecryptnocomments.py
+++ encryptdecryptnocomments.py
@@ -1,31 +1,22 @@
-#definerer alfabetet som en string, hvor a=1, derfor 0 i starten
-#desuden står den igen, det er fordi man på den måde kan få store bogstaver.
 alf = '0abcdefghijklmnopqrstuvwxyzæøåABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ !?,.'
 
-
-#definer forskellige vigtige funktioner
 p = 17
 q = 11
 
 m = p*q
 
-#eulers phifunktion
 phim = (p-1) * (q-1)
 
-#skal være indbyrdes primisk med phim
 k = 7
 if(phim%k == 0):
     print('phim og k er ikke indbyrdes primiske')
 else:
     pass
 
-#blokstørrelse skal defineres. ved blokstørrelse 1 skal der tages 2 tal, derfor ganges med 2
 blokstørrelse = 1
 blok = blokstørrelse*2
 
 
-#brug find fuctionen til at finde ud af hvilken værdi tallene har
-#denne variabel er til at skrive, hvad der skal enkrypteres
 message = 'Citizen X'
 print('Tekst som skal krypteres: ' + message + '\n')
 print('krypteres med blokstoerrelse '+str(blokstørrelse)+
@@ -33,7 +24,6 @@ print('krypteres med blokstoerrelse '+str(blokstørrelse)+
 
 plainmessage = ''
 
-#her tilføjer jeg alle talenne til plaintext variablen
 for a in message:
     if alf.index(a)<10:
         plainmessage += '0'
@@ -41,18 +31,13 @@ for a in message:
     else:
         plainmessage += str(alf.index(a))
 
-#denne er til for at sørge for at alting er splittet op i par, da jeg bruger blokstørrelse 1
-#den skal måske forstås lidt bedre
 plainmessage = [plainmessage[a:a+blok] for a in range(0, len(plainmessage), blok)]
 
-#denne laver det fra en list til en integer, som jeg da kan regne med
 plainmessage = list(map(int, plainmessage))
 
 print("Dette er den ikke-krypterede klartekst:")
 print(plainmessage[0:len(plainmessage)])
 print('\n')
-
-#enkrypteret tekst variabel
 
 encmessage = plainmessage.copy()
 
@@ -62,30 +47,18 @@ print("Dette er den krypterede tekst:")
 print(encmessage)
 print('\n')
 
-#her kommer euklids algoritme-delen
-#her skal der findes 2 tal, sådan at k*u-phim*v=1
-
-
-#her defineres en funktion, som retunerer extended greatest common devisor
 def usfd(a, b):
-    """return (g, x, y) such that a*x + b*y = g = sfd(a, b)"""
     x0, x1, y0, y1 = 0, 1, 1, 0
     while a != 0:
-        #divmod er en funktion som dividerer argument 1 med argument 2, og printer (resultat, remainder)
         (q, a), b = divmod(b, a), a
         y0, y1 = y1, y0 - q * y1
         x0, x1 = x1, x0 - q * x1
     return b, x0, y0
 
-#her definerer jeg 3 variabler, da den outputter 3 resultater. Det eneste jeg skal bruge er u, som ganges på første led
-#De andre 2 definerer jeg også, men de bliver ikke brugt til noget.
 useless = 0
 u = 0
 v = 0
 useless, u, v = usfd(k,phim)
-
-#her kommer så selve dekrypteringen, som følger meget det samme som enkrypteringen, men nye tal
-#endnu en ny variabel, denne gang klarteksten, som da kan stykkes sammen til tekst
 
 decmessage = encmessage.copy()
 
@@ -95,7 +68,6 @@ print('dette er den dekrypterede besked:')
 print(decmessage)
 print('\n')
 
-#her laver jeg en variabel, som til sidst bliver ordet igen
 dectext = ''
 
 for a in decmessage:
